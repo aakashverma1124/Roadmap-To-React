be9dce24ce3f5a792dff56efa284ca3b81741bd5
aakashverma1124 <info.aakash11@gmail.com>
2020-07-09 19:53:40 +0530

    Adding our first functional component using arrow functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Greet from "./greet";
+import Greet from "./components/greet";
 
 class App extends Component {
     render() {
