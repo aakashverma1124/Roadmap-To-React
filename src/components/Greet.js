11fa64c92e15a226a452cd7804b847eabc2cfbc4
aakashverma1124 <info.aakash11@gmail.com>
2020-07-10 15:39:30 +0530

    Differentiating JSX and JavaScript Syntax...

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -7,6 +7,26 @@ import React from 'react'
 // }
 
 // Try it using arrow functions.
-const Greet = () => <h1>Hello, Geek!</h1>
+const Greet = () =>  {
+    // return (
+    //     <div>
+    //         <h1>Hello, Geek!</h1>
+    //     </div>
+    // )
+
+    return (
+        React.createElement (
+            'div',
+            null,
+            React.createElement (
+                'h1',
+                null,
+                'Hello, World!'
+            )
+        )
+    )
+}
+
+
 
 export default Greet
